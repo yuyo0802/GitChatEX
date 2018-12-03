9e70694a181f21ac07bb8dc507fcfe57de6ce63b
yuyo0802 <45280891+yuyo0802@users.noreply.github.com>
2018-12-03 21:27:59 +0800

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -21,6 +21,7 @@ scores = data["Score"].values
 X_train = X[:11]
 X_test = X[11:]
 
+"""
 # Linear Regression - Regression
 y_train = scores[:11]
 y_test = scores[11:]
@@ -29,6 +30,40 @@ regr = LinearRegression()
 regr.fit(X_train, y_train)
 y_predict = regr.predict(X_test)
 
-# 对比测试结果和预期结果
-print(regr.score(X_test, y_test))
+print(y_predict)
+"""
+"""
+# Logistic Regression – Binary Classification
+passed = []
+
+for i in range(len(scores)):
+    if(scores[i] >= 60):
+        passed.append(1)
+    else:
+        passed.append(0)
+
+y_train = passed[:11]
+y_test = passed[11:]
+"""
+
+# Logistic Regression - Multiple Classification
+level = []
+
+for i in range(len(scores)):
+    if(scores[i] >= 85):
+        level.append(2)
+    elif(scores[i] >= 60):
+        level.append(1)
+    else:
+        level.append(0)
+
+y_train = level[:11]
+y_test = level[11:]
+
+
+classifier = LogisticRegression(C=1e5)
+classifier.fit(X_train, y_train)
+
+y_predict = classifier.predict(X_test)
+print(y_predict)
 
